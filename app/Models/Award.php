a556503e21f4293ba68804e48b6e74427154db2f
dhanyn10 <dhanyn.dhan@gmail.com>
2022-09-01 20:51:05 +0700

    tambah model:award

---
+++ app/Models/Award.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Award extends Model
+{
+    use HasFactory;
+
+    protected $table        = "award";
+    protected $primaryKey   = "id";
+    public $incrementing    = true;
+    public $timestamps      = false;
+
+    protected $fillable = [
+        "name",
+        "type",
+        "poin"
+    ];
+}
