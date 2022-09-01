917c414a1286d09d6dd3ae24c90aea0bc55d0757
dhanyn10 <dhanyn.dhan@gmail.com>
2022-09-01 13:22:54 +0700

    update tampilan welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,11 @@
 @extends('layout')
 @section('content')
-    welcome
+<div class="row">
+  <div class="col-md-4 offset-md-4 text-center">
+    {{config('app.name')}}
+    <p>enter your email address<br/>to sign in and continue</p>
+    <input type="text" name="" id="" placeholder="Email Address" class="form-control mb-3"/>
+    <button class="btn btn-secondary">Sign In</button>
+  </div>
+</div>
 @endsection
