7f9d605fcdc8825370d0eb659cda66db63db9f5f
dhanyn10 <dhanyn.dhan@gmail.com>
2022-09-02 15:52:15 +0700

    middleware

--- app/Http/Controllers/AwardController.php
+++ app/Http/Controllers/AwardController.php
@@ -10,42 +10,35 @@ class AwardController extends Controller
 {
     public function show(Request $request)
     {
-        if(session()->has('email'))
-        {
-            $data = null;
-            $poin = (int) $request->input('poin');
-            $type = $request->input('type');
-            if($poin != "" && $type != "")
-            {
-                $type = explode(",", $type);
-    
-                $valtype = [];
-                for($i = 0; $i < count($type); $i++)
-                {
-                    if(strtolower($type[$i]) =='voucher')
-                        array_push($valtype, 1);
-                    elseif(strtolower($type[$i]) =='product')
-                        array_push($valtype, 2);
-                    elseif(strtolower($type[$i]) =='giftcard')
-                        array_push($valtype, 3);
-                }
+        $data = null;
+        $poin = (int) $request->input('poin');
+        $type = $request->input('type');
         
-                $data = DB::table('award')
-                        ->where('poin', '>=', $poin)
-                        ->whereIn('type', $valtype)->paginate(6);
-            }
-            else
+        if($poin != "" && $type != "")
+        {
+            $type = explode(",", $type);
+
+            $valtype = [];
+            for($i = 0; $i < count($type); $i++)
             {
-                $data = Award::paginate(6);
+                if(strtolower($type[$i]) =='voucher')
+                    array_push($valtype, 1);
+                elseif(strtolower($type[$i]) =='product')
+                    array_push($valtype, 2);
+                elseif(strtolower($type[$i]) =='giftcard')
+                    array_push($valtype, 3);
             }
-            return view('award', [
-                'dataAward' => $data
-            ]);
+    
+            $data = DB::table('award')
+                    ->where('poin', '>=', $poin)
+                    ->whereIn('type', $valtype)->paginate(6);
         }
         else
         {
-            flash('you need to login', 'danger');
-            return redirect()->route('welcome');
+            $data = Award::paginate(6);
         }
+        return view('award', [
+            'dataAward' => $data
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,16 @@ use App\Http\Controllers\CrudController;
 use App\Http\Controllers\LogoutController;
 
 Route::get('/', [WelcomeController::class, 'show'])->name('welcome');
-Route::get('award', [AwardController::class, 'show'])->name('award');
-Route::get('add', [CrudController::class, 'showAdd'])->name('add');
-Route::get('auto-add', [CrudController::class, 'autoAdd'])->name('auto-add');
-Route::get('logout', [LogoutController::class, 'logout'])->name('logout');
 Route::post('/', [WelcomeController::class, 'login']);
-Route::post('add', [CrudController::class, 'formAdd']);
-Route::post('award', [AwardController::class, 'show']);
+
+Route::group([
+  'middleware' => 'user'
+], function () {
+
+  Route::get('award', [AwardController::class, 'show'])->name('award');
+  Route::get('add', [CrudController::class, 'showAdd'])->name('add');
+  Route::get('auto-add', [CrudController::class, 'autoAdd'])->name('auto-add');
+  Route::post('add', [CrudController::class, 'formAdd']);
+  Route::post('award', [AwardController::class, 'show']);
+});
+Route::get('logout', [LogoutController::class, 'logout'])->name('logout');
