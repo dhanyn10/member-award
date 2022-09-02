538cccdba8ac43dadbf72f72b20783a66c4d2da5
dhanyn10 <dhanyn.dhan@gmail.com>
2022-09-02 17:19:23 +0700

    update tampilan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,9 @@
 @extends('layout')
 @section('content')
 <div class="row">
-  <div class="col-md-4 offset-md-4 text-center">
-    {{config('app.name')}}
+  <div class="col-md-4 offset-md-4 mt-4 text-center">
+    <i class="fa fa-star text-warning" style="font-size: 10em;" aria-hidden="true"></i>
+    <h1 style="font-weight: bolder">{{strtoupper(config('app.name'))}}</h1>
     @if (flash()->message)
         <div class="alert alert-{{ flash()->class }}" role="alert">
           {{ flash()->message }}
@@ -12,7 +13,7 @@
     <form method="post">
       {{ csrf_field() }}
       <input type="text" name="email" placeholder="Email Address" class="form-control mb-3"/>
-      <button type="submit" class="btn btn-secondary">Sign In</button>
+      <button type="submit" class="btn btn-secondary rounded-0">Sign In</button>
     </form>
   </div>
 </div>
